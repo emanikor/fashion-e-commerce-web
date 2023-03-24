a11e12e5d96fe48da7a9469306603949c801902b
emanikor <87859876+emanikor@users.noreply.github.com>
2023-03-24 14:39:57 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -31,3 +31,37 @@ var swiper = new Swiper(".home-slider", {
       },
    loop:true,
   });
+
+//   menu 
+const menuBtns = document.querySelectorAll('.menu-btn');
+const foodItems = document.querySelectorAll('.card');
+
+let activeBtn = "featured";
+
+showFoodMenu(activeBtn);
+
+menuBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        resetActiveBtn();
+        showFoodMenu(btn.id);
+        btn.classList.add('active-btn');
+    });
+});
+
+function resetActiveBtn(){
+    menuBtns.forEach((btn) => {
+        btn.classList.remove('active-btn');
+    });
+}
+
+function showFoodMenu(newMenuBtn){
+    activeBtn = newMenuBtn;
+    foodItems.forEach((item) => {
+        if(item.classList.contains(activeBtn)){
+            item.style.display = "grid";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+
