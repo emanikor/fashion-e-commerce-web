c13f85a4b0e848b3b79cebf49971a01169171116
emanikor <87859876+emanikor@users.noreply.github.com>
2023-04-05 21:48:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -85,32 +85,115 @@ cart.onclick =() =>{
 // close cart
 closeCart.onclick =() =>{
     console.log('clicked')
-    cart.classList.remove('active');
+    cartBox.classList.remove('active');
 };
 
 // cart working
-  if(document.readystate == 'loading'){
+  if(document.readyState == 'loading'){
     document.addEventListener('DOMContentLoaded', ready);
   }
   else{
-    ready()
-       
-
+    ready();
     }
-//   making function
 
+//   making function
 function ready(){
     // remove item from cart
-    var removeCartBtton =document.getElementsByClassName('cart-remove');
-    console.log(removeCartBtton);
-    for(var i =0; i< removeCartBtton.length; i++){
-        var button =removeCartBtton[i];
-        button.addEventListener('click' , removeCartBtton);
+    var removeCartBton =document.getElementsByClassName('cart-remove');
+    console.log(removeCartBton);
+    for(var i =0; i< removeCartBton.length; i++){
+        var button =removeCartBton[i];
+        button.addEventListener('click', removeCartItem);
+    }
+// quantity changed 
+    var quantityInputs =document.getElementsByClassName('cart-quantity')
+    for(var i =0; i< quantityInputs.length; i++){
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged);
     }
 }
+// ADD cart
+var addCart = document.getElementsByClassName('fa-cart-shopping')
+for (var i =0; i< addCart.length; i++){
+  var button =addCart[i]
+  button.addEventListener('click', addCartClicked); 
+}
+
 
 // remove items from cart{
     function removeCartItem(event){
  var buttonClicked = event.target;
  buttonClicked.parentElement.remove();
+       updateTotal(); 
+    }
+
+    // quantity changes
+    function  quantityChanged(event){
+        var input = event.target  
+        if (isNaN(input.value) || input.value <=0){
+            input.value = 1;
+        }
+        updateTotal();
+    }
+
+
+    //  add to cart
+   function  addCartClicked(event){
+    var button =event.target
+    var shopProducts = button.parentElement
+    var title = shopProducts.querySelectorAll('h3,price')[0].innerText;
+    var price = shopProducts.getElementsByClassName('price')[0].innerText;
+    var productImg = shopProducts.querySelectorAll('img')[0].src;
+    addProductToCart(title, price, productImg );
+    updateTotal();
+   
+   }
+ 
+   function addProductToCart(title, price, productImg){
+    var cartShopBox = document.createElement('div')
+    // cartShopBox.classList.add('cart-boxItem')
+    var cartItems = document.getElementsByClassName('cart-content')[0]
+    var cartItemsNames = cartItems.getElementsByClassName('cart-product-title')
+    for (var i =0; i< cartItemsNames.length; i++){
+        alert('you have already add this item')
+        return;
+    }
+   
+   }
+
+   var  cartBoxContent =`
+   <img src="img/card1.png" alt="" class="cart-img">
+   <div class="detail-box">
+     <div class="cart-product-title">Dress</div>
+     <div class="cart-price">ksh74</div>
+     <input type="number" value="1" class="cart-quantity">
+   </div>
+   <!-- remove -->
+   <button class="cart-remove">del</button> `;
+
+
+   cartShopBox.innerHTML = cartBoxContent
+   cartItems.append(cartShopBox)
+   cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem)
+   cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('click', quantityChanged)
+
+
+
+
+
+    // upload total
+    function updateTotal (){
+        var cartContent = document.getElementsByClassName('cart-content')[0];
+        var cartBoxes =  cartContent.getElementsByClassName('cart-boxItem');
+        var total= 0
+        for(var i =0; i< cartBoxes.length; i++){
+            var cartBox = cartBoxes[i];
+            var priceElement = cartBox.getElementsByClassName('cart-price')[0]
+            var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+            var price = parseFloat(priceElement.innerText.replace("ksh", ""));
+            var quantity = quantityElement.Value 
+            total = total + (price *quantity);
+
+            document.getElementsByClassName('total-price')[0].innerText ="ksh" + total;
+        }
     }
